2e24c4e36625eb0ba043d40b0f99f7955cdeae8e
Navin <navin@osiansoftware.com>
2024-12-12 18:23:19 +0530

    added icon for remote user current status

--- src/ui/LiveVideo.jsx
+++ src/ui/LiveVideo.jsx
@@ -77,8 +77,32 @@ export const LiveVideo = () => {
         {remoteUsers.map((user) => (
           <div
             key={user.uid}
-            className="remote-video-container mx-auto h-full !w-full rounded-xl border border-gray-800"
+            className="relative remote-video-container mx-auto h-full rounded-xl border border-gray-800"
           >
+            <button className="btn absolute top-4 left-4 z-10">
+              {user.hasAudio ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-9 h-9 text-white px-2 rounded"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"
+                  />
+                </svg>
+              ) : (
+                <img
+                  src={MicOff}
+                  alt="Mic off"
+                  className="w-8.5 h-9 bg-red-500 px-2 py-2 rounded"
+                />
+              )}
+            </button>
             <RemoteUser user={user} />
           </div>
         ))}
